9317acb371223ad46e2170f179e0f84394da543b
Rachit Garg <rachitg.code@gmail.com>
2025-11-01 22:39:08 +0530

    fix:vg variable for subscrId

--- src/mssqlserver-aw22/ad-hoc/gacp.sh
+++ src/mssqlserver-aw22/ad-hoc/gacp.sh
@@ -1,4 +1,17 @@
+# gacp(){
+#  git status
+#  git add .
+#  git status
+#  git commit -m "$1"
+#  git status
+#  git push
+#  git status
+# }
+
+	  
 gacp(){
+ git status
+ git pull
  git status
  git add .
  git status
