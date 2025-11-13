0653098b5c6e3c756a623f820604a39757afca92
Rachit Garg <rachitg.code@gmail.com>
2025-11-13 21:04:49 +0530

    improv: counts proc

--- src/mssqlserver-aw22/stored-proc/sp_all_t_cnts.sql
+++ src/mssqlserver-aw22/stored-proc/sp_all_t_cnts.sql
@@ -20,10 +20,17 @@ BEGIN
     WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA <> 'dbo';
 
     -- Declare a table variable to store results
-    DECLARE @Results TABLE (
-        name NVARCHAR(150),
+    DECLARE @Counts TABLE (
+        tableSchema NVARCHAR(150),
+        tableName NVARCHAR(150),
         cnt int
     );
+
+    -- DECLARE @Results TABLE (
+    --     name NVARCHAR(150),
+    --     cnt int,
+    --     inscope int
+    -- );
 
     -- Open the cursor
     OPEN myCursor;
@@ -35,7 +42,7 @@ BEGIN
     WHILE @@FETCH_STATUS = 0
     BEGIN
         -- Build the dynamic SQL query
-        SET @DynamicSQL = N'SELECT ''' + @full_table_name + ''' AS TableName, COUNT(1) AS Cnt FROM ' + @full_table_name;
+        SET @DynamicSQL = N'SELECT ''' + @table_schema + ''' AS tableSchema, ''' + @table_name + ''' AS tableName, COUNT(1) AS Cnt FROM ' + @full_table_name;
 
         SET @full_table_name = QUOTENAME(@table_schema) + '.' + QUOTENAME(@table_name);
 
@@ -43,7 +50,7 @@ BEGIN
         print(@full_table_name);
         -- Execute the query with parameters
         
-        INSERT INTO @Results (name, cnt)
+        INSERT INTO @Counts (tableSchema, tableName, cnt)
         EXEC sp_executesql @DynamicSQL, 
                            N'@f_tname NVARCHAR(100)', 
                            @f_tname = @full_table_name;
@@ -57,12 +64,34 @@ BEGIN
      DEALLOCATE myCursor;
 
 
-    IF @p_sort_flag = 1 
-        BEGIN
-            SELECT REPLACE(REPLACE(name, '[',''),']','') AS name, cnt FROM @Results ORDER BY cnt DESC,name;
-        END
+    IF @p_sort_flag = 1
+    BEGIN
+        SELECT 
+            c.tableSchema,
+            c.tableName,
+            c.cnt,
+            CASE WHEN fdb.SourceTable IS NOT NULL THEN 1 ELSE 0 END AS IsInScope
+        FROM @Counts c
+        LEFT JOIN [FrameworkDb].[dbo].[TableMetadata] AS fdb
+            ON LOWER(TRIM(c.tableSchema)) = LOWER(TRIM(fdb.SourceSchema))
+            AND LOWER(TRIM(c.tableName)) = LOWER(TRIM(fdb.SourceTable))
+            AND fdb.SourceSystem = 'Database'
+            AND fdb.SourceType IN ('OnPremSQL')
+        ORDER BY c.cnt DESC, c.tableSchema, c.tableName;
+    END
     ELSE
-        BEGIN
-            SELECT REPLACE(REPLACE(name, '[',''),']','') AS name, cnt FROM @Results ORDER BY name;
-        END;
+    BEGIN
+        SELECT 
+            c.tableSchema,
+            c.tableName,
+            c.cnt,
+            CASE WHEN fdb.SourceTable IS NOT NULL THEN 1 ELSE 0 END AS IsInScope
+        FROM @Counts c
+        LEFT JOIN [FrameworkDb].[dbo].[TableMetadata] AS fdb
+            ON LOWER(TRIM(c.tableSchema)) = LOWER(TRIM(fdb.SourceSchema))
+            AND LOWER(TRIM(c.tableName)) = LOWER(TRIM(fdb.SourceTable))
+            AND fdb.SourceSystem = 'Database'
+            AND fdb.SourceType IN ('OnPremSQL')
+        ORDER BY c.tableSchema, c.tableName;
+    END;
 END;
